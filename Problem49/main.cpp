03b80132b612638032130099b9514fb3cf9197bf
KAI-LXII <veenstra.kyler@gmail.com>
2024-10-06 19:23:30 -0700

    Init

---
+++ Problem49/main.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ret;
+        for (int i = 0; i < strs.size(); i++) {
+            bool successfullyFoundList = false;
+            for (int j = 0; j < ret.size(); j++) {
+                if (makeAlpha(strs.at(i)) == makeAlpha(ret.at(j).front())) {
+                    successfullyFoundList = true;
+                    ret.at(j).push_back(strs.at(i));
+                }
+            }
+
+            if (!successfullyFoundList) {
+                vector<string> newList;
+                newList.push_back(strs.at(i));
+                ret.push_back(newList);
+            }
+            
+        }
+        return ret;
+    }
+
+    string makeAlpha(string str) {
+        std::sort(str.begin(), str.end());
+        return str;
+    }
+};
